1183c5c8d600d3b17fe67fce7e4e50e85a2231e4
Dhiraj Chaudhari <dhirajchaudhari789@gmail.com>
2023-08-17 04:46:32 +0530

    Added button and assigned onClick property
    onclick it will render "Click me" in the console.

--- app.jsx
+++ app.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 const App = () => {
+  const changeTheVal = () => {
+    console.log("Click me");
+  }
   return (
     <div>
       <h1>Hello world.</h1>
+      <button onClick={()=> changeTheValue}</button>>Click me</button>
     </div>
   )
 }
